f253ff122a22a6a3459c158a585cca4d7091a7e4
Lucas <lrigon2904@gmail.com>
2022-07-14 16:43:25 -0300

    finalização

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-const count = [0,0,0,0,0,0,0,0,0,0,0]
-
-const results = document.getElementById('graph')
-const roll = document.getElementById('roll')
+//const results = document.getElementById('graph')
+//const roll = document.getElementById('roll')
 
 function random(min, max){
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 
-roll.addEventListener('click', diceRoll)
+//roll.addEventListener('click', diceRoll)
 
 function diceRoll(){
     const count = [0,0,0,0,0,0,0,0,0,0,0]
     let access = 0
     let countRoll = 1
-    results.innerHTML = ""
-    while(countRoll <= 1000){
+    //results.innerHTML = ""
+    while(countRoll <= 5){
         let dice_1 = random(1,6)
         let dice_2 = random(1,6)
         let sum = dice_1 + dice_2
         count[sum-2] = count[sum-2] + 1
         countRoll += 1
+        console.log(`${count}       ${access}      ${countRoll}     ${dice_1}      ${dice_2}      ${sum}`)
     }
     for(let i = 2; i < count.length + 2; i++){
         const div = document.createElement('div')
@@ -30,11 +29,6 @@ function diceRoll(){
         results.appendChild(div)
         access += 1
     }
-    console.log(count)
     return count
 }
-
-
-
-
-
+diceRoll()
